b5328f20b3c03a66000a736f4e31d7c6cdc513ff
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-19 23:40:45 +0900

    DTL

--- TemplateProject/TemplateApp/views.py
+++ TemplateProject/TemplateApp/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 def index(request):
     val = 'Good Bye'
     return render(request, 'index.html', context={'value': val})
+
+def home(request):
+    my_name = 'Taro Tamada'
+    favorite_fruits = ['apple', 'grape', 'lemon']
+    my_info = {
+        'name' : 'Taro',
+        'age' : 18
+    }
+
+    return render(request, 'home.html', context={
+        'my_name': my_name,
+        'favorite_fruits': favorite_fruits,
+        'my_info': my_info
+    })
