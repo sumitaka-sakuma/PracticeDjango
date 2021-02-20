8f18ed04508326c4dead9c0a6da7e2f8a5a3e817
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-02-20 15:56:41 +0900

    created template myself

--- TemplateProject/TemplateApp/views.py
+++ TemplateProject/TemplateApp/views.py
@@ -11,11 +11,13 @@ def home(request):
         'name' : 'Taro',
         'age' : 18
     }
+    status = 20
 
     return render(request, 'home.html', context={
         'my_name': my_name,
         'favorite_fruits': favorite_fruits,
-        'my_info': my_info
+        'my_info': my_info,
+        'status': status
     })
 
 def sample1(request):
@@ -46,3 +48,4 @@ def sample(request):
         'msg': msg,
         'msg2': msg2
     })
+
